fb71d41189654386cd2424eefa3ffdaf44576beb
Leni Aniva <v@leni.sh>
2025-08-08 22:09:47 -0700

    refactor(test): Use `lspecEachIO`

--- Test/Main.lean
+++ Test/Main.lean
@@ -18,20 +18,21 @@ namespace Pantograph.Test
 def addPrefix (pref: String) (tests: List (String × α)): List (String  × α) :=
   tests.map (λ (name, x) => (pref ++ "/" ++ name, x))
 
-/-- Runs test in parallel. Filters test name if given -/
-def runTestGroup (nameFilter?: Option String) (tests: List (String × IO LSpec.TestSeq)): IO LSpec.TestSeq := do
-  let tests: List (String × IO LSpec.TestSeq) := match nameFilter? with
+abbrev TestTask := Task (Except IO.Error LSpec.TestSeq)
+def filterTestGroup (tests : List (String × IO LSpec.TestSeq)) (nameFilter? : Option String)
+  : IO (List (String × TestTask)) := do
+  let tests : List (String × IO LSpec.TestSeq) := match nameFilter? with
     | .some nameFilter => tests.filter (λ (name, _) => nameFilter.isPrefixOf name)
     | .none => tests
-  let tasks: List (String × Task _) ← tests.mapM (λ (name, task) => do
-    return (name, ← EIO.asTask task))
-  let all := tasks.foldl (λ acc (name, task) =>
-    let v: Except IO.Error LSpec.TestSeq := Task.get task
-    match v with
-    | .ok case => acc ++ (LSpec.group name case)
-    | .error e => acc ++ (expectationFailure name e.toString)
-    ) LSpec.TestSeq.done
-  return all
+  tests.mapM λ (name, t) => return (name, ← IO.asTask t)
+
+/-- Runs test in parallel. Filters test name if given -/
+def runTestTask (t : (String × TestTask)) : IO LSpec.TestSeq := do
+  let (name, task) := t
+  let v: Except IO.Error LSpec.TestSeq := task.get
+  return match v with
+  | .ok case => LSpec.group name case
+  | .error e => expectationFailure name e.toString
 
 end Pantograph.Test
 
@@ -68,4 +69,4 @@ def main (args: List String) := do
     f env_default
   let tests : List (String × IO LSpec.TestSeq) := suites.foldl (init := []) λ acc (name, suite) =>
     acc ++ (addPrefix name $ suiterunner suite)
-  LSpec.lspecEachIO [()] (λ () => runTestGroup nameFilter? tests)
+  LSpec.lspecEachIO (← filterTestGroup tests nameFilter?) runTestTask
